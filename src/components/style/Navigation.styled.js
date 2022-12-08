555708190cf77bdd6008b87ac36ea1b6cccc6e1c
unknown <mishomishiko123@gmail.com>
2022-12-08 12:47:35 +0400

    character

--- src/components/style/Navigation.styled.js
+++ src/components/style/Navigation.styled.js
@@ -3,7 +3,6 @@ import styled from "styled-components";
 const Nav = styled.nav`
   width: 1340px;
   height: 100px;
-  border-bottom: 2px solid #625e5e17;
   display: flex;
   margin: 0 auto;
   align-items: center;
